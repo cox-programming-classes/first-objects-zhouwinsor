a7b449f288cec1a4cd013b59095fbff507888a8d
zhouwinsor <56124717+zhouwinsor@users.noreply.github.com>
2024-02-08 12:47:32 -0500

    class 2/8/2024

--- FirstObjects_2024/Decks.cs
+++ FirstObjects_2024/Decks.cs
@@ -1,12 +1,14 @@
 using System.Collections;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstObjects_2024;
 /// <summary>
 /// represents a standard deck of playing cards
 /// </summary>
-public class Deck:IEnumerable<Card> {
+public class Deck :IEnumerable<Card>{
+    
     private List<Card> _cards;
-
+    
     private Random rnd = new Random();
     // check if there are cards left in the deck:
     public bool IsEmpty => _cards.Count == 0;
@@ -117,6 +119,46 @@ public class Deck:IEnumerable<Card> {
     #endregion Enumerables.
 }
 
+public class Hand
+{
+    private List<Card> _hand;
+
+    public Hand() { _hand = []; }
+
+    //static makes it so you can access it from any instance of deck, and it is independent of what other values are
+    private static readonly Random rnd = new(); 
+    public void Add(Card c) { _hand.Add(c); }
+    public Card Take()
+    {
+        if (_hand.Count == 0)
+        {
+            throw new InvalidOperationException("Deck is Empty!");
+        }
+
+        var card = _hand.First();
+        _hand.RemoveAt(0);
+        return card;
+    }
+
+    public Card TakeRandomly()
+    {
+        if (_hand.Count == 0)
+        {
+            throw new InvalidOperationException("Deck is Empty!");
+        }
+
+        var n = rnd.Next(_hand.Count);
+        var card = _hand[n];
+        _hand.RemoveAt(n);
+        return card;
+    }
+
+    public bool Has(Suit s) => _hand.Any(card => card.Suit== s);
+    
+    public bool Has(Value v) => _hand.Any(card => card.Value== v);
+    
+    public bool Has(Card c) => _hand.Any(card => card == c);
+}
 
 
 
